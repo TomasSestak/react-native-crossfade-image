e6cb3211de932e0478da76b5d4a285d295fa38c4
sadboi <tomassestak@seznam.cz>
2023-05-16 16:44:03 +0200

    patch

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useLayoutEffect, useState } from 'react';
+import React, { useCallback, useLayoutEffect, useRef, useState } from 'react';
 import {
   Animated,
   Easing,
@@ -19,25 +19,33 @@ export interface CrossfadeImageProps extends ImageProps {
 }
 
 export const CrossfadeImage: React.FC<CrossfadeImageProps> = ({
-  style,
-  source,
-  duration = 500,
-  easing = Easing.ease,
-  children,
-  ...props
-}) => {
+                                                                style,
+                                                                source,
+                                                                duration = 500,
+                                                                easing = Easing.ease,
+                                                                children,
+                                                                ...props
+                                                              }) => {
   const prevSource = usePrevious(source);
   const [oldSource, setOldSource] = useState<ImageSourcePropType>(source);
   const [newSource, setNewSource] = useState<ImageSourcePropType>();
   const [opacity] = useState(() => new Animated.Value(0));
 
+  const ongoingAnimation = useRef(false);
+  const setNextSource = useRef(() => {});
+
   useLayoutEffect(() => {
     if (!isEqual(source, prevSource)) {
+      if (ongoingAnimation.current) {
+        setNextSource.current = () => setNewSource(source);
+        return;
+      }
       setNewSource(source);
     }
   }, [source, prevSource]);
 
   const handleLoad = useCallback(() => {
+    ongoingAnimation.current = true;
     Animated.timing(opacity, {
       toValue: 1,
       duration,
@@ -53,28 +61,31 @@ export const CrossfadeImage: React.FC<CrossfadeImageProps> = ({
   const handleUpdate = useCallback(() => {
     setNewSource(undefined);
     opacity.setValue(0);
+    ongoingAnimation.current = false;
+    setNextSource.current();
+    setNextSource.current = () => {};
   }, [opacity]);
 
   return (
-    <View style={[styles.root, style]}>
-      <Image
-        {...props}
-        style={styles.image}
-        source={oldSource}
-        fadeDuration={0}
-        onLoad={handleUpdate}
-      />
-      {newSource && (
-        <Animated.Image
-          {...props}
-          style={[styles.image, { opacity }]}
-          source={newSource}
-          fadeDuration={0}
-          onLoad={handleLoad}
+      <View style={[styles.root, style]}>
+        <Image
+            {...props}
+            style={styles.image}
+            source={oldSource}
+            fadeDuration={0}
+            onLoad={handleUpdate}
         />
-      )}
-      {children}
-    </View>
+        {newSource && (
+            <Animated.Image
+                {...props}
+                style={[styles.image, { opacity }]}
+                source={newSource}
+                fadeDuration={0}
+                onLoad={handleLoad}
+            />
+        )}
+        {children}
+      </View>
   );
 };
 
